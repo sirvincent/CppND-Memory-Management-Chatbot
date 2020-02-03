7ca68bcabe82408411622fe98ac9bd46c911105d
Vincent Post <v.post@condifood.com>
2020-02-03 08:07:57 +0100

    RFT: use C++14 make_unique

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -126,7 +126,7 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     std::cout << "Constructor of ChatBotPanelDialog is called" << std::endl;
 
     // create chat logic instance
-    _chatLogic.reset(new ChatLogic());
+    _chatLogic = std::make_unique<ChatLogic>();
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
